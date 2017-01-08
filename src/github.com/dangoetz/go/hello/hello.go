ecc1e2235eb68db5112586a6133fbfcd708b051d
Daniel Goetz <dan.goetz@gmail.com>
2017-01-08 15:59:50 -0500

    testing various functions.

--- src/github.com/dangoetz/go/hello/hello.go
+++ src/github.com/dangoetz/go/hello/hello.go
@@ -7,7 +7,17 @@ func main() {
 	fmt.Printf("nada, que vola amigo?\n")
 	dan()
 }
+
+// dan is the main jump function that I'll be using like main()
 func dan() {
 	fmt.Printf("\nnot much brother!\n")
 	fmt.Printf("how about you?\n")
+}
+
+func testing() {
+	fmt.Println("Entering testing...")
+}
+
+func doOtherStuff() {
+
 }
