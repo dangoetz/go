64ecca3445683641bb44fd3b31e3dd009e5616ad
Daniel Goetz <dan.goetz@gmail.com>
2017-01-08 15:38:27 -0500

    sort of getting it...

--- src/github.com/dangoetz/go/hello/hello.go
+++ src/github.com/dangoetz/go/hello/hello.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	fmt.Printf("hello, Danny Boy, what's new?\n")
 	fmt.Printf("nada, que vola amigo?\n")
+	dan()
+}
+func dan() {
+	fmt.Printf("\nnot much brother!\n")
+	fmt.Printf("how about you?\n")
 }
---
+++ src/github.com/dangoetz/go/hello/wzup/wzup.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// Dan function: This is a test
+func Dan() {
+	fmt.Printf("\nnot much brother!\n")
+	fmt.Printf("how about you?\n")
+}
